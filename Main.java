ab45a5321367b6cffa2eee73b81d8408a2d6a43a
AlloysiusM <albrentmanlutac@gmail.com>
2025-01-27 23:04:08 +1300

    switched to javafx

---
+++ Main.java
@@ -0,0 +1,32 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("pages/loginscene.fxml"));
+
+            AnchorPane root = loader.load();
+
+            Controller controller = loader.getController();
+            controller.setMainWindow(primaryStage);
+
+            Scene scene = new Scene(root, 1080, 720);
+
+            primaryStage.setResizable(false);
+            primaryStage.setTitle("MazeBank");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/Controller.java
@@ -0,0 +1,12 @@
+import javafx.stage.Stage;
+
+public class Controller {
+    private Stage mainStage;
+
+    // This method sets the main Stage object
+    public void setMainWindow(Stage primaryStage) {
+        this.mainStage = primaryStage;
+    }
+
+    // Add other methods or handlers as needed
+}
